55ac905fac14f16b03e52d2c842f40fabda6dd2c
Small6oy <gggndlovu@gmail.com>
2024-01-12 10:30:35 +1100

    added quicksort big o

--- Sort/quickSort.js
+++ Sort/quickSort.js
@@ -24,6 +24,8 @@ function pivot(arr, start = 0, end = arr.length - 1) {
 }
 
 
+//Worst Case Time Complexity O(n log n)
+//Worst Case Space Complexity 0(n)
 function quickSort(arr, left = 0, right = arr.length - 1) {
     if (left < right) {
         let pivotIndex = pivot(arr, left, right) //3
